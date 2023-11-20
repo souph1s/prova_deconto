5bcadf214a006b61eeecd171f614eb997f54ebe0
Sophia <sophiamperez@hotmail.com>
2023-11-20 19:40:49 -0300

    funcoes de API ok

--- API/Program.cs
+++ API/Program.cs
@@ -23,6 +23,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(
+    c => c.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+);
+
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
